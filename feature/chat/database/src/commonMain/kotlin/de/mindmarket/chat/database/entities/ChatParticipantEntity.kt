25ff1e270e36af5ad4dd3e38e4a83aec0057418a
Michael <michaelwidlok@gmail.com>
2026-01-05 17:39:29 +0100

    creating the db schema

---
+++ feature/chat/database/src/commonMain/kotlin/de/mindmarket/chat/database/entities/ChatParticipantEntity.kt
@@ -0,0 +1,12 @@
+package de.mindmarket.chat.database.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class ChatParticipantEntity(
+    @PrimaryKey
+    val userId: String,
+    val username: String,
+    val profilePictureUrl: String?
+)
