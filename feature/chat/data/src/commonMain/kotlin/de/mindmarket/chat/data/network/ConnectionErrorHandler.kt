6377c621ba355f9a07ac420047ade11dabfac88a
Michael <michaelwidlok@gmail.com>
2026-03-06 19:23:20 +0100

    implementing connection error handler

---
+++ feature/chat/data/src/commonMain/kotlin/de/mindmarket/chat/data/network/ConnectionErrorHandler.kt
@@ -0,0 +1,9 @@
+package de.mindmarket.chat.data.network
+
+import de.mindmarket.chat.domain.models.ConnectionState
+
+expect class ConnectionErrorHandler {
+    fun getConnectionStateForError(cause: Throwable): ConnectionState
+    fun transformException(exception: Throwable): Throwable
+    fun isRetriableError(cause: Throwable): Boolean
+}
---
+++ feature/chat/data/src/iosMain/kotlin/de/mindmarket/chat/data/network/IOSNetworkCancellationException.kt
@@ -0,0 +1,6 @@
+package de.mindmarket.chat.data.network
+
+class IOSNetworkCancellationException(
+    message: String,
+    cause: Throwable?
+) : Exception(message, cause)
